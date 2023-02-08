b8977bb12af77e06a38bca63062aefb73822ba53
scripsi <scripsi@scripsi.uk>
2023-02-08 14:45:50 +0000

    Remove Node PM2 config

--- conf/scandl.rc.d
+++ conf/scandl.rc.d
@@ -7,6 +7,7 @@
 # Add the following lines to /etc/rc.conf to enable ocrmypdf:
 #
 #scandl_enable="YES"
+#scandl_user="username"
 
 . /etc/rc.subr
 
@@ -14,7 +15,7 @@ name="scandl"
 rcvar="scandl_enable"
 
 command="/usr/sbin/daemon"
-command_args="-S -l daemon -s debug -T scandl -u user -p /var/run/scandl.pid /usr/bin/env -i OCR_INPUT_DIRECTORY=/home/user/scandl/in OCR_OUTPUT_DIRECTORY=/home/user/scandl/out OCR_ON_SUCCESS_DELETE=1 HOME=/home/user PATH=/usr/local/bin:${PATH} USER=user /usr/local/bin/python /home/user/scandl/bin/watcher.py"
+command_args="-S -l daemon -s debug -T scandl -u ${scandl_user} -p /var/run/scandl.pid /usr/bin/env -i OCR_INPUT_DIRECTORY=/home/${scandl_user}/scandl/in OCR_OUTPUT_DIRECTORY=/home/${scandl_user}/scandl/out OCR_ON_SUCCESS_DELETE=1 HOME=/home/${scandl_user} PATH=/usr/local/bin:${PATH} USER=${scandl_user} /usr/local/bin/python /home/${scandl_user}/scandl/bin/watcher.py"
 
 
 load_rc_config ${name}
